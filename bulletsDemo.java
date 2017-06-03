e3e44b560043339e4911e96aa4786187f2a5e603
codarsnacht <joe97gleeson@gmail.com>
2017-06-03 12:13:31 +0100

    Effective Rate of Fire of weapon
    Allows user to fine the effective rate of fire along with other info like time to fire 1 bullet , time to empty a magazine

---
+++ bulletsDemo.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+/**
+ * 
+ */
+
+/**
+ * @author Joe Gleeson
+ *
+ */
+public class bulletsDemo {
+
+	/**
+	 * @param args
+	 */
+	// I could have put split this into 2 seperate classes but since it is quite a small program , there was no need 
+	//find out how many bullets you fire from a weapon in a minute 
+	// enter rate of fire and then program will calculate
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Please enter Rate of Fire(rounds per minute) of weapon:");
+		Scanner scan = new Scanner(System.in);//scans for input
+		double RoF = scan.nextDouble();//gets base rate of fire
+	
+		
+		System.out.println("Please enter rounds in magazine:") ;
+		Scanner scan1 = new Scanner(System.in);
+		int mag = scan.nextInt(); // gets mag size
+		
+		int bullet = 0 ;
+		double rate=0;
+		//gets time per 1 bullet is fired
+		rate = (60/RoF); 
+		System.out.println("The weapon fires a bullet every " + rate+ " seconds");
+		
+		double fired=0;
+		int bulletCount=0; // counts bullets fired 
+		//time expected until mag is empty;
+		//stops when count hits the number of rounds in mag
+		for (int count=0; count <mag; count++ ){
+			fired = rate + fired;
+			 bulletCount++;
+			System.out.println("bullet fired are " + bulletCount+ ", time passed is " + fired) ;
+		}
+		System.out.println("Mag is empty");//says when mag is empty
+	
+		System.out.println("Enter reload speed ;");
+		Scanner scan2 = new Scanner(System.in);//gets reload speed
+		double reload = scan2.nextDouble();
+		
+		
+		double time = reload + fired; // gets total time of mag empty + reload
+		double total = 60/time ;//gets amount of times you will be able to fire with reload and firing weapon per minute
+		System.out.println("Total time would be " + total ) ;
+	
+		double efective = total*fired;// gets the effective time you are firing the weapon 
+		System.out.println("Total effective time Firing the weapon would be " + efective ) ;
+		double RateEff = efective / rate; // gets effective rate of fire with reload taken into account
+		System.out.println("Effective rate of fire is " + RateEff + " rounds per minute, which is " + (int)RateEff+ " bullets per minute" );
+	}
+
+}
